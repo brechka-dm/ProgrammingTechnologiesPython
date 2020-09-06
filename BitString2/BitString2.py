7b8d48eb022d4ee6fd6fbe828085c27310461203
Den Brechka <dbrechkawork@yandex.ru>
2020-09-06 13:32:18 +0600

    lab 2 with files

--- BitString2/BitString2.py
+++ BitString2/BitString2.py
@@ -1,4 +1,6 @@
-'''---INPUT FUNCTION---'''
+import sys #for command line arguments proceed
+
+'''---MANUAL INPUT FUNCTION---'''
 def inp():
     res=input('Input bit string : ')
     if len(res)>8: 
@@ -11,6 +13,27 @@ def inp():
     suffixlen=8-len(res)
     suffix='0'*suffixlen
     res=suffix+res
+    return res
+    
+'''---FILE INPUT FUNCTION---'''
+def inp(filename):
+    try: #exception handling
+        file=open(filename, 'r') #trying to open file to read
+    except: #in case of file open fault
+        print('Unable to open file')
+        exit()
+    res=file.readline() #read one line from file
+    if len(res)>8: 
+        print('Incorrect input')
+        exit()
+    for b in res: 
+        if b!='0' and b!='1':
+            print('Incorrect input')
+            exit()
+    suffixlen=8-len(res)
+    suffix='0'*suffixlen
+    res=suffix+res
+    file.close()
     return res
 
 '''---CONJUNCTION FUNCTION---'''
@@ -26,11 +49,23 @@ def conj(a, b):
     return c
 
 '''---MAIN---'''
-'''---input---'''
-bs1=inp()
-bs2=inp()
+'''---manual input---'''
+#bs1=inp()
+#bs2=inp()
+'''---file input---'''
+if len(sys.argv) <3: #check arguments count
+    print('Not enough parameters')
+    print('BitString2 a.txt b.txt c.txt')
+    exit()
+bs1=inp(sys.argv[1])
+bs2=inp(sys.argv[2])
 '''---conjunction---'''
 bs3=conj(bs1,bs2)
-'''---output---'''
-print('Result: ',bs3)
+'''---manual output---'''
+#print('Result: ',bs3)
+'''---file output---'''
+res=open(sys.argv[3],'w')
+res.write(bs3)
+res.close()
+
 
