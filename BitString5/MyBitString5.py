7331041974502cdfe2d519ee28b9880f60815f18
brechka-dm <dbrechkawork@yandex.ru>
2020-12-03 09:02:34 +0600

    add mas_from_string

--- BitString5/MyBitString5.py
+++ BitString5/MyBitString5.py
@@ -1,41 +1,44 @@
 class MyBitString:
-    def inp(self): #manual input
-        self.mas=input('Input bit string : ') #mas is object member
-        for b in self.mas: 
+    def mas_from_str(self, st):
+        self.mas=""
+        for b in st: 
             if b!='0' and b!='1':
                 print('Incorrect input')
                 exit()
+            else:
+                self.mas+=(b)
+ 
+    def inp(self): 
+        m=input('Input bit string : ')
+        self.mas_from_str(m)
     
-    def f_inp(self,filename): #file input
+    def f_inp(self,filename):
         try:
             file=open(filename, 'r')
         except:
             print('Unable to open file')
             exit()
-        self.mas=file.readline()
-        for b in self.mas: 
-            if b!='0' and b!='1':
-                print('Incorrect input')
-                exit()
+        m=file.readline()
+        self.mas_from_str(m)
         file.close()
 
-    def outp(self): #console output
+    def outp(self): 
         print(self.mas)
         
-    def f_outp(self, filename): #file output
+    def f_outp(self, filename): 
         res=open(filename,'w')
         res.write(self.mas)
         res.close()        
 
-    def conj(self, b): #b is a MyBitString object
-        c='' #c is string
-        mi=self.mas #determine max and min strings
+    def conj(self, b): 
+        c='' 
+        mi=self.mas 
         ma=b.mas
         if len(ma)<len(mi):
             mi=ma
             ma=self.mas
-        sl=len(ma)-len(mi) #both strings must be equal bby length
-        suf='0'*sl #add previous 0s to smallest string
+        sl=len(ma)-len(mi) 
+        suf='0'*sl 
         mi=suf+mi
         i=0
         while (i<len(ma)): 
@@ -44,16 +47,12 @@ class MyBitString:
             else:
                 c+='0'
             i+=1
-        c=c[c.find('1'):] #delete extra 0s from the begin 
-        return MyBitString(c) #return MyBitString object by calling constructor
+        c=c[c.find('1'):] 
+        return MyBitString(c) 
         
-    def __init__(self, init_str=None): # constructor
-        if init_str!=None: #if init_str==None it works like default constructor
-            self.mas=init_str
-            for b in self.mas: 
-                if b!='0' and b!='1':
-                    print('Incorrect input')
-                    exit()
+    def __init__(self, init_str=None): 
+        if init_str!=None: 
+            self.mas_from_str(init_str)
 
     def __and__(self, b): #overloading &
         return self.conj(b)
